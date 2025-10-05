c7295abdce52abeee6f59b664a146ae9bbef6b5b
marina almeida <tenorioalmeidamarina@gmail.com>
2025-10-05 02:29:12 -0300

    criando responsividade  com js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
 const form = document.getElementById('form');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const weight = document.getElementById('weight').value;
+    const height = document.getElementById('height').value;
+
+    const bmi= (weight / (height * height)).toFixed(2);
+
+    const value = document.getElementById('value');
+    let description = '';
+
+    document.getElementById('info').classList.remove('hidden');
+
+
+});
